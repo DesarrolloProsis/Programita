5975397b756299427d7d150cc8c37f8a7bb8123d
La Mera Chuleta <geoffreyytorres25@gmail.com>
2019-05-16 11:26:52 -0500

    inicio

---
+++ ConsoleApp2/Models.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Models
+    {
+        public class Primero
+        {
+            public long Id { get; set; }
+            public string Tag { get; set; }
+        }
+
+        public class Segundo
+        {
+            public long Id { get; set; }
+            public string NumTag { get; set; }
+            public double Saldo { get; set; }
+            public double saldoViejo { get; set; }
+            public double saldoNuevo { get; set; }
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp2.Models;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string SQL = "Data Source =.; Initial Catalog = GTDB; Integrated Security = True";
+
+            SqlConnection ConexionSQL = new SqlConnection(SQL);
+
+            using (SqlCommand SqlCommand = new SqlCommand("", ConexionSQL))
+            {
+
+                try
+                {
+
+                    ConexionSQL.Open();
+                    SqlCommand.ExecuteNonQuery();
+                    SqlDataAdapter sqlData = new SqlDataAdapter(SqlCommand);
+                    DataTable table = new DataTable();
+                    sqlData.Fill(table);
+                    List<Primero> Lista1 = new List<Primero>();
+
+                    foreach(DataRow item in table.Rows)
+                    {
+                        Lista1.Add(new Primero
+                        {
+                            Id = Convert.ToInt64(item["Id"]),
+                            Tag = Convert.ToString(item["Tag"])
+
+                        });
+                    }   
+                    
+
+                    foreach(var item2 in Lista1)
+                    {
+                        SqlCommand.CommandText = "Select CuentaId, NumTag, NumCuenta, StatusTag, StatusCuenta, TypeCuenta, SaldoCuenta, SaldoTag " +
+                                                   "From Tags t Inner Join CuentasTelepeajes c on t.CuentaId = c.Id Where t.NumTag = '" + item2.Tag + "'";
+
+                        SqlCommand.ExecuteNonQuery();
+                        SqlDataAdapter sqlData2 = new SqlDataAdapter(SqlCommand);
+                        DataTable table2 = new DataTable();
+                        sqlData2.Fill(table2);
+
+                        if (table2.Rows.Count != 0)
+                        {
+
+
+
+                            if (table2.Rows[0]["TypeCuenta"].ToString() == "Individual")
+                            {
+
+                                SqlCommand.CommandText = "Select Sum(CAST(Monto as float)) from OperacionesCajeroes where Numero = '" + item2.Tag + "'";
+                                double SaldoTotal = Convert.ToDouble(SqlCommand.ExecuteScalar());
+
+                                SqlCommand.CommandText = "Select Id, Saldo, SaldoAnterior, SaldoActualizado from Historico where Tag = '" + item2.Tag +"'";
+
+                                SqlCommand.ExecuteNonQuery();
+                                SqlDataAdapter sqlData3 = new SqlDataAdapter(SqlCommand);
+                                DataTable table3 = new DataTable();
+                                sqlData3.Fill(table3);
+
+                                List<Segundo> Lista2 = new List<Segundo>();
+
+                                foreach(DataRow item3 in table3.Rows)
+                                {
+                                    Lista2.Add(new Segundo
+                                    {
+                                        Id = Convert.ToInt64(item3["Id"]),
+                                        NumTag = Convert.ToString(item3["Tag"]),
+                                        Saldo = Convert.ToDouble(item3["Saldo"]),
+                                        saldoNuevo = Convert.ToDouble(item3["SaldoAnterior"]),
+                                        saldoViejo = Convert.ToDouble(item3["SaldoActualizado"])
+
+                                    });
+                                }
+                                
+                                foreach(var item4 in Lista2)
+                                {
+                                    if (item4.saldoViejo == 0 && item4.saldoNuevo == 0)
+                                    {
+                                        SqlCommand.CommandText = "update Historico set SaldoAnterior = '" + Convert.ToString(SaldoTotal) + "' where Id = '" + item4.Id + "'";
+                                        SqlCommand.ExecuteNonQuery();
+                                        SaldoTotal = SaldoTotal - item4.Saldo;
+                                        SqlCommand.CommandText = "update Historico set SaldoAnterior = '" + Convert.ToString(SaldoTotal) + "' where Id = '" + item4.Id + "'";
+                                    }                                    
+
+
+                                }
+                              
+
+
+                            }
+                            else
+                            {
+                                var Cuenta = table2.Rows[0]["NumCuenta"].ToString();
+                                var CuentaID = table2.Rows[0]["CuentaId"].ToString();
+
+                                SqlCommand.CommandText = "Select Sum(CAST(Monto as float)) from OperacionesCajeroes where Numero = '"+Cuenta+"'";
+                                double SaldoTotal = Convert.ToDouble(SqlCommand.ExecuteScalar());
+
+                                SqlCommand.CommandText = "Select h.Id,, h.Tag, h.Saldo, h.SaldoAnterior, SaldoActualizado from Tags t inner join Historico h on t.NumTag = h.Tag where t.CuentaId = '" + CuentaID + "'";
+
+                                SqlCommand.ExecuteNonQuery();
+                                SqlDataAdapter sqlData4 = new SqlDataAdapter(SqlCommand);
+                                DataTable table4 = new DataTable();
+                                sqlData4.Fill(table4);
+
+                                List<Segundo> Lista3 = new List<Segundo>();
+
+                                foreach (DataRow item5 in table4.Rows)
+                                {
+                                    Lista3.Add(new Segundo
+                                    {
+                                        Id = Convert.ToInt64(item5["Id"]),
+                                        NumTag = Convert.ToString(item5["Tag"]),
+                                        Saldo = Convert.ToDouble(item5["Saldo"]),
+                                        saldoNuevo = Convert.ToDouble(item5["SaldoAnterior"]),
+                                        saldoViejo = Convert.ToDouble(item5["SaldoActualizado"])
+
+                                    });
+                                }
+
+                                foreach (var item6 in Lista3)
+                                {
+                                    if (item6.saldoViejo == 0 && item6.saldoNuevo == 0)
+                                    {
+                                        SqlCommand.CommandText = "update Historico set SaldoAnterior = '" + Convert.ToString(SaldoTotal) + "' where Id = '" + item6.Id + "'";
+                                        SqlCommand.ExecuteNonQuery();
+                                        SaldoTotal = SaldoTotal - item6.Saldo;
+                                        SqlCommand.CommandText = "update Historico set SaldoAnterior = '" + Convert.ToString(SaldoTotal) + "' where Id = '" + item6.Id + "'";
+                                    }
+
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+                    
+
+                }
+                catch(Exception ex)
+                {
+
+                }
+
+                ConexionSQL.Close();
+
+            }
+
+        }
+    }
+}
